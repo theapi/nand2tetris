7d44f38e23873e627d33464047171d7b1a84dc52
theapi <petermclarke@gmail.com>
2015-07-04 17:27:37 +0100

    Start of Mult (no loop yet)

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,9 +1,28 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @sum // location of memory holding the running total
+  M=0 // initialise to zero ( sum = 0 )
+
+  @R0 // load the value of R0 into M
+  D=M // Set D to the value of R0
+  
+  @i // init a variable
+  M=D // set i to have the value of R0 ( i = R0 )
+  
+  // Loop R0 number of times whilst adding R1 to the running total
+  // @todo the actual loop!
+  @R1 // load the value of R1 into M
+  D=M // Set D to the value of R1
+  
+  @sum // load the sum into M
+  D = D + M // Add R1 to the total
+  
+  @sum
+  M=D // remember the result
+
+  @R1 // load the value of R1 into M
+  D=M // Set D to the value of R1
+  
+  @sum // load the sum into M
+  D = D + M // Add R1 to the total
