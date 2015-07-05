1fbbcf013b7628884495a9a34106b18e552c8781
theapi <petermclarke@gmail.com>
2015-07-05 12:41:56 +0100

    Mult, working

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,28 +1,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-  @sum // location of memory holding the running total
-  M=0 // initialise to zero ( sum = 0 )
+  @R2 // location of memory holding the running total
+  M=0 // initialise result & current data to zero
+ 
+  @R1 // the location of the iteration value
+  D=M // Set data to be that of R1, ready for the loop comparison
+    
+  // Loop R1 number of times whilst adding R0 to the running total
+  
+(LOOP)
+  
+  @END // Load the address of the END instruction
+  D;JEQ // If R1 == 0 then goto END
 
   @R0 // load the value of R0 into M
   D=M // Set D to the value of R0
   
-  @i // init a variable
-  M=D // set i to have the value of R0 ( i = R0 )
+  // ADD R2, R0
+  @R2
+  M=D+M // store the result back into R2
   
-  // Loop R0 number of times whilst adding R1 to the running total
-  // @todo the actual loop!
-  @R1 // load the value of R1 into M
-  D=M // Set D to the value of R1
+  // Decrement R1
+  @R1
+  MD=M-1 // Set R1 and the data register at the same time
   
-  @sum // load the sum into M
-  D = D + M // Add R1 to the total
+  @LOOP // Get the address of the loop
+  0;JMP // Goto LOOP
   
-  @sum
-  M=D // remember the result
+(END)
+  @END // get ready to jump to here
+  0;JMP // Infinite loop
 
-  @R1 // load the value of R1 into M
-  D=M // Set D to the value of R1
   
-  @sum // load the sum into M
-  D = D + M // Add R1 to the total
